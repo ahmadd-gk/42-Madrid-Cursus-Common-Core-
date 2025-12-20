b4ff0a5675f321c79427764c8c7e8959357140d9
Ahmad Gutierrez Kasem <ahgutier@c1r1s5.42madrid.com>
2025-12-20 20:41:54 +0100

    a

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,54 +6,72 @@
 /*   By: ahgutier <ahgutier@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 16:36:59 by ahgutier          #+#    #+#             */
-/*   Updated: 2025/11/19 16:36:59 by ahgutier         ###   ########.fr       */
+/*   Updated: 2025/12/20 19:32:03 by ahgutier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
 	static char	*stash;
+	char		*line;
 
-	stash = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
 	stash = ft_read_stash(fd, stash);
+	if (!stash)
+		return (NULL);
+	line = ft_extract_line(stash);
+	if (!line)
+	{
+		free(stash);
+		stash = NULL;
+		return (NULL);
+	}
+	stash = ft_update_stash(stash);
+	return (line);
 }
 
-char	*update_stash(char	*stash)
+char	*ft_update_stash(char *stash)
 {
-	char	*new_stashbaby;
+	char	*new_stash;
 	int		nl_index;
-	int		i;
-	int		j;
+	size_t	len;
+	size_t	i;
+	size_t	j;
 
+	if (!stash)
+		return (NULL);
 	nl_index = ft_find_newline(stash);
 	if (nl_index == -1)
 	{
 		free(stash);
 		return (NULL);
 	}
-	new_stashbaby = malloc(ft_strlen(stash) - nl_index);
-	if (!new_stashbaby)
+	len = ft_strlen(stash);
+	new_stash = malloc(len - nl_index);
+	if (!new_stash)
 		return (NULL);
-	i = nl_index + 1; // Para que se salte el '\n' NO para ell null terminator
-	while (stash[i])
-		new_stashbaby[j++] = stash[i++];
-	new_stashbaby[j] = '\0';
+	i = (size_t)nl_index + 1;
+	j = 0;
+	while (i < len)
+		new_stash[j++] = stash[i++];
+	new_stash[j] = '\0';
 	free(stash);
-	return (new_stashbaby);
+	return (new_stash);
 }
 
-size_t	ft_strlcpy_gnl(char	*dst, char	*src, size_t	size)
+size_t	ft_strlcpy_gnl(char *dst, const char *src, size_t size)
 {
-	size_t	i;
+	size_t i;
 
 	if (!dst || !src)
 		return (0);
 	i = 0;
 	if (size > 0)
 	{
-		while (src[i] && i < size - 1)
+		while (src[i] && i + 1 < size)
 		{
 			dst[i] = src[i];
 			i++;
@@ -65,10 +83,29 @@ size_t	ft_strlcpy_gnl(char	*dst, char	*src, size_t	size)
 	return (i);
 }
 
-char	*ft_read_stash(int	fd, char	*stash)
+char	*ft_read_stash(int fd, char *stash)
 {
-	char	*buff[BUFFER_SIZE + 1];
-	ssize_t	*bytes_read;
+	char	buf[BUFFER_SIZE + 1];
+	ssize_t	bytes_read;
 
-	bytes_read = read(fd, buff, BUFFER_SIZE);
+	bytes_read = 1;
+	while (ft_find_newline(stash) == -1 && bytes_read > 0)
+	{
+		bytes_read = read(fd, buf, BUFFER_SIZE);
+		if (bytes_read < 0)
+		{
+			free(stash);
+			return (NULL);
+		}
+		buf[bytes_read] = '\0';
+		stash = ft_strjoin_gnl(stash, buf);
+		if (!stash)
+			return (NULL);
+	}
+	return (stash);
 }
+
+/*
+	len = ft_strlen(stash);
+	// tamaño de lo que queda = len - (nl_index + 1) ; +1 para '\0' -> malloc(len - nl_index)
+*/
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,23 +6,25 @@
 /*   By: ahgutier <ahgutier@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 18:18:25 by ahgutier          #+#    #+#             */
-/*   Updated: 2025/11/25 18:18:25 by ahgutier         ###   ########.fr       */
+/*   Updated: 2025/12/20 20:39:24 by ahgutier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t ft_strlen(char *s)
+size_t	ft_strlen(char const *s)
 {
-	size_t len;
+	size_t	len;
 
+	if (!s)
+		return (0);
 	len = 0;
-	while (*s++)
+	while (s[len])
 		len++;
 	return (len);
 }
 
-ssize_t find_newline(char *s)
+ssize_t	ft_find_newline(char *s)
 {
 	size_t i;
 
@@ -32,18 +34,20 @@ ssize_t find_newline(char *s)
 	while (s[i])
 	{
 		if (s[i] == '\n')
-			return (i);
+			return ((ssize_t)i);
 		i++;
 	}
 	return (-1);
 }
 
-char *ft_init_stash(char const *buff)
+char	*ft_init_stash(const char *buff)
 {
 	size_t	len;
-	char 	*new;
+	char	*new;
 
-	len = ft_strlen(buff);
+	if (!buff)
+		return (NULL);
+	len = ft_strlen((char *)buff);
 	new = malloc(len + 1);
 	if (!new)
 		return (NULL);
@@ -53,45 +57,48 @@ char *ft_init_stash(char const *buff)
 
 char *ft_strjoin_gnl(char *stash, char const *buff)
 {
-	char 	*res;
-	size_t 	len1;
-	size_t 	len2;
+	char	*res;
+	size_t	len1;
+	size_t	len2;
 
 	if (!buff)
-		return (stash);
+	    return (stash);
 	if (!stash)
 		return (ft_init_stash(buff));
 	len1 = ft_strlen(stash);
 	len2 = ft_strlen(buff);
 	res = malloc(len1 + len2 + 1);
 	if (!res)
+	{
+		free(stash);
 		return (NULL);
+	}
 	ft_strlcpy_gnl(res, stash, len1 + 1);
 	ft_strlcpy_gnl(res + len1, buff, len2 + 1);
 	free(stash);
 	return (res);
 }
 
-char	*ft_extract_line(char	*stash)
+char *ft_extract_line(char *stash)
 {
-	char *nl_line;
+	char	*line;
 	int nl_index;
 	int i;
 
-	if (!stash || stash[0] == '\0')
+	if (!stash || !stash[0])
 		return (NULL);
 	nl_index = ft_find_newline(stash);
 	if (nl_index == -1)
-		nl_index = ft_strlen(stash) - 1;
-	nl_line = (char *)malloc(nl_index + 2);
-	if (!nl_line)
+		nl_index = ft_strlen(stash);
+	line = malloc(nl_index + 2);
+	if (!line)
 		return (NULL);
 	i = 0;
-	while (i <= nl_index)
+	while (i <= nl_index && stash[i])
 	{
-		nl_line[i] = stash[i];
+		line[i] = stash[i];
 		i++;
 	}
-	nl_line[i] = '\0';
-	return (nl_line);
+	line[i] = '\0';
+	return (line);
 }
